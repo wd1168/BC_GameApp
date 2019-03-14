d6565fa1f7713064f53d1d0517cc9a1e4f4d0eea
em1404 <43792117+em1404@users.noreply.github.com>
2019-03-13 23:44:42 -0400

    Update Login.php

--- web/Login.php
+++ web/Login.php
@@ -4,5 +4,55 @@
  *
  * @package Example-application
  */
-require_once "../configuration/config.php";
+   require_once "../configuration/config.php";
+    require_once "../configuration/dbconfig.php";
+
+
+	if(isset($_GET['login'])) {
+        $errMsg = '';
+        $msg = '';
+		// Get data from FORM
+		$username = $_GET['username'];
+        $password = $_GET['password'];
+
+        if($username == '')
+			$errMsg = 'Enter username <br/>';
+		if($password == '')
+			$errMsg .= ' Enter password';
+		if($errMsg == '') {
+
+            $query = "SELECT User_ID, First_Name, Last_Name, `Password` FROM user WHERE First_Name = '$username';";
+
+            $statement = $pdo->prepare($query);
+
+            $statement ->execute();
+
+                try 
+                    {
+                        $row = $statement->fetch(PDO::FETCH_ASSOC);
+                       if (($row['First_Name'] == $username) && ($password == $row['Password']))
+                        {
+                            
+                            $msg = "Log in successful!";
+                        } 
+                        else
+                        {
+                            $errMsg = "Incorrect username or password";
+                        }
+                    }
+                    catch(PDOException $e)
+                        {
+                        echo "Error: " . $e->getMessage();
+                        }
+
+                    }
+
+
+                }
+                    $pdo = null;
+
+    
+    $smarty -> assign('errMsg', $errMsg);
+    $smarty -> assign('msg', $msg);
+ 
 $smarty->display('Login.tpl');
