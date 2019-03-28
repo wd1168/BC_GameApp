d57de7dbef751548b1317dac0fbfd6dc61e154f4
wd1168 <wd1168@messiah.edu>
2019-03-28 13:13:22 -0400

    fixed rule pulling

--- web/game_rules.php
+++ web/game_rules.php
@@ -1,26 +1,6 @@
 <?php
 
 
-if( $_GET['link']=='gofish' ){
-    $game_name = "gofish";
-} 
-else if( $_GET['link']=='solitaire' ){
-    $game_name = "solitaire";
-}
-else if( $_GET['link']=='war' ){
-    $game_name = "war";
-}
-else if( $_GET['link']=='battleship' ){
-    $game_name = "battleship";
-}
-else if( $_GET['link']=='othello' ){
-    $game_name = "othello";
-}
-else{
-    $game_name = "monopoly";
-}
-
-
 require_once "../configuration/config.php";
 require_once "../configuration/dbconfig.php";
 
@@ -41,15 +21,12 @@ if (isset($_SESSION['User'])){
     $smarty -> assign('last_name', $lname);
 }
 
-
-
-
+$game_name = $_GET['link'];
 
 $rule_query = "SELECT Rule, `Name`, `Type`, Age, Player_Count, Category, Subcategory FROM game_rule
 INNER JOIN game ON game_rule.Game_ID=game.Game_ID where game.Name = '$game_name';";
 
 $statement2 = $pdo->prepare($rule_query);
-
 $statement2 ->execute();
 
 try 
