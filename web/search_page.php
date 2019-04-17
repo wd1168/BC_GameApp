85095402a1448fb7b1155fb83287bab5b5b32e0a
Ben <bu1157@messiah.edu>
2019-04-17 16:35:03 -0400

    Include search.php

--- web/search_page.php
+++ web/search_page.php
@@ -2,44 +2,6 @@
 require_once "../configuration/config.php";
 require_once "../configuration/dbconfig.php";
 
-session_start();
-if (isset($_SESSION['User'])){
-    $query = "SELECT First_Name, Last_Name FROM user
-          WHERE User_ID = :id";
-
-    $statement = $pdo->prepare($query);
-    $statement->bindParam(':id', $_SESSION['User']);
-    $statement ->execute();
-
-    $result = $statement->fetch(PDO::FETCH_ASSOC);
-    $fname = $result["First_Name"];
-    $lname = $result['Last_Name'];
-
-    $smarty -> assign('first_name', $fname);
-    $smarty -> assign('last_name', $lname);
-}
-
-
-$search_query = "SELECT `Name` FROM game;";
-
-$statement2 = $pdo->prepare($search_query);
-
-$statement2 ->execute();
-
-try 
-    {
-        $games = array();
-
-        while ($row = $statement2->fetch(PDO::FETCH_ASSOC)) {
-            array_push($games, $row['Name']);
-        }
-        
-    }
-    catch(PDOException $e)
-        {
-        echo "Error: " . $e->getMessage();
-        }
-    $pdo = null;
-    $smarty -> assign('games', $games);
+include 'search.php';
  
 $smarty->display('search_page.tpl');
