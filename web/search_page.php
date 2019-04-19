24be65f8973e259905395633503b6e952a3067fd
em1404 <43792117+em1404@users.noreply.github.com>
2019-04-18 22:45:00 -0400

    Update search_page.php

--- web/search_page.php
+++ web/search_page.php
@@ -3,11 +3,20 @@
 $search_query = "SELECT `Name` FROM game;";
 $search_statement = $pdo->prepare($search_query);
 $search_statement ->execute();
+
+$search_q = "SELECT Rule FROM game_rule;";
+$search_stmt = $pdo->prepare($search_q);
+$search_stmt ->execute();
 try 
     {
         $search_results = array();
         while ($row = $search_statement->fetch(PDO::FETCH_ASSOC)) {
             array_push($search_results, $row['Name']);
+        }
+
+        $r_results = array();
+        while ($row = $search_stmt->fetch(PDO::FETCH_ASSOC)) {
+            array_push($r_results, $row['Rule']);
         }
         
     }
@@ -17,3 +26,4 @@ try
         }
 
 $smarty -> assign('search_results', $search_results);
+$smarty -> assign('r_results', $r_results);
